360437b3ddb655a81edd8e1faa39a5b5488a458e
Manuel Reyes <drakanez@gmail.com>
2023-02-16 15:04:20 -0500

    Added nodejs express and nodemon dependencies

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const express =  require('express');
+const app = express();
 
 // 127.0.0.1 is the loopback address
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello New Paltz!")
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening at http://${hostname}:${port}`);
+})
+
+/*
 const server = http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -13,3 +24,4 @@ const server = http.createServer((req,res) => {
 server.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
+*/
