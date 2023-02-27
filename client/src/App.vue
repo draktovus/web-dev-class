05d14bc6213e400e77d23c4069400292e05d4986
Manuel Reyes <drakanez@gmail.com>
2023-02-27 14:42:16 -0500

    Cleaned up Vue example code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 	import { RouterLink, RouterView } from 'vue-router'
-	import HelloWorld from './components/HelloWorld.vue'
 	let obj = {
 		msg: "Hello from New Paltz!"
 	}
