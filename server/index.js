5b7013e1ddd4e6e5eae4cf453417809246ed7721
Manuel Reyes <drakanez@gmail.com>
2023-03-21 12:09:35 -0400

    started server api

--- server/index.js
+++ server/index.js
@@ -1,27 +1,20 @@
 const http = require('http');
 const express =  require('express');
+const products = require('./controllers/products')
 const app = express();
+
 
 // 127.0.0.1 is the loopback address
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello New Paltz from nodemon!")
-})
+app
+    .get('/', (req, res) => {
+        res.send("Hello New Paltz from Express!")
+    })
+    .use('/products', products)
+
 
 app.listen(port, ()=>{
     console.log(`Listening at http://${hostname}:${port}`);
 })
-
-/*
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello New Paltz!\n');
-});
-
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-});
-*/
