7c055ab7c7e7b463d47ded9907b0ce28734868f3
Manuel Reyes <drakanez@gmail.com>
2023-05-01 15:37:34 -0400

    google login fix

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import { loadScript,rest } from '@/model/myFetch';
 
+
+async function googleLogin()
+{
+    await loadScript('https://accounts.google.com/gsi/client', 'google-login')
+
+    const client = google.accounts.oauth2.initTokenClient({
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        scope: 'https://www.googleapis.com/auth/calendar.readonly \
+                https://www.googleapis.com/auth/contacts.readonly',
+        callback: async (tokenResponse) => {
+            console.log(tokenResponse)
+
+            const me = await rest('https://people.googleapis.com/v1/people/me?personFields=name,emailAddresses',
+            null, undefined,
+            {
+                "Authorization": "Bearer " + tokenResponse.access_token 
+            }
+            )
+        },
+    });
+    client.requestAccessToken();
+}
 </script>
 
 <template>
@@ -11,6 +34,11 @@
             If you got here then you are not logged in. <br>
             To login just click the login button.
         </h2>
+        <p>
+            <button class="button is-primary" @click="googleLogin">
+                Login With Google
+            </button>
+        </p>
     </div>
 </template>
 
